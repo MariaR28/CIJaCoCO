a4fcfc2d1582dc4f7044b51dee7f234a5e7ea059
Maria R <maria.repina1@gmail.com>
2024-05-16 12:56:20 +0700

    Second tets is added

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -2,6 +2,7 @@ package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+
 public class StatisticsServiceTest {
 
     @Test
@@ -10,6 +11,17 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void findAnotherMax() {
+        ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 60, 11, 11, 12};
+        long expected = 60;
 
         long actual = service.findMax(incomesInBillions);
 
